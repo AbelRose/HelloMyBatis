1b1c1ab7d0ad352509d232710c4519e08e313575
Matrix <5419159+abelrose@user.noreply.gitee.com>
2020-12-13 13:31:58 +0800

    1213

---
+++ Test/com/matrix/day01/TestMyBatis.java
@@ -0,0 +1,11 @@
+package com.matrix.day01;
+
+import org.junit.Test;
+
+public class TestMyBatis {
+
+    @Test
+    public void test() {
+
+    }
+}
---
+++ src/com/matrix/day01/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.matrix.day01.mapper;
+
+import com.matrix.day01.bean.User;
+
+public interface UserMapper {
+    User getUserByUid(String id);
+}
